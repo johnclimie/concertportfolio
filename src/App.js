e905c737b5f3bd9d3770516b15a82956ce15728e
John Climie <johnclimie00@gmail.com>
2022-05-19 12:54:19 -0400

    Added styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './style.css'
 function App() {
   return (
     <div className="App">
-      <h1>My Concert Photo Portfolio</h1>
+      <header>
+        <h1>My Concert Photo Portfolio</h1>
+      </header>
     </div>
   );
 }
