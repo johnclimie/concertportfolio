a7b142d43747ae05717c72c64d6d7493d1a03a9a
John Climie <johnclimie00@gmail.com>
2022-05-25 14:13:14 -0400

    Styled content

--- src/App.js
+++ src/App.js
@@ -13,20 +13,22 @@ function App() {
         <div id='about'>
           <h1 id='welcome-title'>Welcome To My Concert Photo Portfolio </h1>
           
-          <h2>Over the years, I have been to several different concerts, and seen several different artists.</h2>
+          <div id='welcome-content'>
+            <h2>Over the years, I have been to several different concerts, and seen several different artists.</h2>
 
-          <h2>In fact, I have</h2>
+            <h2>In fact, I have</h2>
 
-          <ul>
-            <li>Been to 5 concerts and 1 festival</li>
-            <li>Seen 32 different artists</li>
-            <li>Visited 3 different venues</li>
-            <li>Taken dozens of amazing photos</li>
-          </ul>
+            <ul>
+              <li>Been to 5 concerts, 1 Sunday Service and 1 festival</li>
+              <li>Seen 32 different artists</li>
+              <li>Visited 3 different venues</li>
+              <li>Taken dozens of amazing photos</li>
+            </ul>
 
-          <h2>Therefore, I have created this website, which can help showcase the moments I've captured during these musical events.</h2>
+            <h2>Therefore, I have created this website, which can help showcase the moments I've captured during these musical events.</h2>
 
-          <h2>Here, you can select a concert I've been to, and view the several different photos I've taken during the event.</h2>
+            <h2>Here, you can select a concert I've been to, and view the several different photos I've taken during the event.</h2>
+          </div>
         </div>
         
 
