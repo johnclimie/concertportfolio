c009bbb1d11cacef28df1cef71062fb1770396d0
John Climie <johnclimie00@gmail.com>
2022-05-25 13:15:21 -0400

    Set up header that could work

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './style.css'
 function App() {
   return (
     <div className="App">
-      
+      <nav id='header'>
+        <h1 id='page-title'>Concert Portfolio</h1>
+      </nav>
     </div>
   );
 }
