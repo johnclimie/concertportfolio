403fe3395e28d6696535cb14ac4dba6417758f66
John Climie <johnclimie00@gmail.com>
2022-05-24 20:47:43 -0400

    Started over on design

--- src/App.js
+++ src/App.js
@@ -3,18 +3,7 @@ import './style.css'
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1 id="header">My Concert Photo Portfolio</h1>
-      </header>
-
-      <section id="welcome">
-        <div id="about">
-          <h1>Welcome</h1>
-        </div>
-        <div id="selection">
-          <h1>Selection</h1>
-        </div>
-      </section>
+      
     </div>
   );
 }
