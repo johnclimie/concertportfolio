cc961e3667b179038d718bfb96d5b621a1a03596
John Climie <johnclimie00@gmail.com>
2022-06-10 11:29:53 -0400

    Fixed text decoration and included links

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -10,21 +10,23 @@ export default function Selection({ musicEvents }) {
           and the name of the artist will load, along with the date of the concert, and maybe the venue name*/}
 
           {musicEvents.map((musicEvent) => (
-              <div className="option">
-              <img src={musicEvent.thumbnail} id='selection-img' alt={musicEvent.name}></img>
-  
-              <div className='img-info-overlay'>
-                <div className='img-title'>{musicEvent.name}</div>
-                <p className='img-desc'>{musicEvent.venue}</p>
-                <p className='img-date'>{musicEvent.date}</p>
-              </div>
+              <a href={'/' + musicEvent.path}>
+                <div className="option">
+                  <img src={musicEvent.thumbnail} id='selection-img' alt={musicEvent.name}></img>
+      
+                  <div className='img-info-overlay'>
+                    <div className='img-title'>{musicEvent.name}</div>
+                    <p className='img-desc'>{musicEvent.venue}</p>
+                    <p className='img-date'>{musicEvent.date}</p>
+                  </div>
 
-              <div className='img-info-sub'>
-                <p className='img-title'>{musicEvent.name}</p>
-                <p className='img-desc'>{musicEvent.venue}</p>
-                <p className='img-date'>{musicEvent.date}</p>
-              </div>
-            </div>
+                  <div className='img-info-sub'>
+                    <p className='img-title'>{musicEvent.name}</p>
+                    <p className='img-desc'>{musicEvent.venue}</p>
+                    <p className='img-date'>{musicEvent.date}</p>
+                  </div>
+                </div>
+              </a>
           ))}
         </div>
     )
--- src/events.js
+++ src/events.js
@@ -4,48 +4,56 @@ const musicEvents = [
         date: 'November 25th, 2017',
         venue: 'The Tabernacle',
         thumbnail: 'https://images.complex.com/complex/image/upload/c_fill,dpr_auto,f_auto,fl_lossy,g_face,q_auto,w_1280/gcqlizpztkdckiz1zhms.jpg',
+        path: '6lack'
     },
     {
         name: 'Travis Scott',
         date: 'November 13th, 2018',
         venue: 'State Farm Arena',
         thumbnail: 'http://exclaim.ca/images/travisscott.jpeg',
+        path: 'travis-scott-1',
     },
     {
         name: 'Travis Scott',
         date: 'March 22nd, 2019',
         venue: 'State Farm Arena',
         thumbnail: 'https://townsquare.media/site/812/files/2018/07/travis-scott-astroworld-cover-art-full.jpg',
+        path: 'travis-scott-2',
     },
     {
         name: 'Kanye West',
         date: 'September 15th, 2019',
         venue: 'New Birth Missionary Baptist Church',
         thumbnail: 'https://upload.wikimedia.org/wikipedia/en/a/a4/Kanye_West_-_Jesus_Is_King.png',
+        path: 'kanye-west',
     },
     {
         name: 'James Blake',
         date: 'October 15th, 2021',
         venue: 'The Tabernacle',
         thumbnail: 'https://media.pitchfork.com/photos/60f9880e4a319e50a860a52e/1:1/w_600/James-Blake.jpg',
+        path: 'james-blake'
     },
     {
         name: 'Tyler, The Creator',
         date: 'March 25th, 2022',
         venue: 'Gas South Arena',
         thumbnail: 'https://resources.tidal.com/images/9db5f3a2/b872/45d6/8dbb/2b7a078be19e/640x640.jpg',
+        path: 'tyler-the-creator',
     },
     {
         name: 'Nine Inch Nails',
         date: 'April 30th, 2022',
         venue: 'Shaky Knees Festival',
         thumbnail: 'http://www.bleaq.com/wp-content/uploads/russel-mills-01.jpg',
+        path: 'nine-inch-nails',
     },
     {
         name: 'Phoebe Bridgers',
         date: 'May 27th, 2022',
         venue: 'Cadence Bank Amphitheatre',
         thumbnail: 'https://media.pitchfork.com/photos/5ee923f47bb7acb328d5683d/1:1/w_600/Punisher%20_Phoebe%20Bridgers.jpg',
+        path: 'phoebe-bridgers',
     },
     
 ]
