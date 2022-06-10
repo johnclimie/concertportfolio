0c2c04c88b3efe2dfc18e4c270e572370a3aa7cd
John Climie <johnclimie00@gmail.com>
2022-06-10 10:55:38 -0400

    Set-Up Browser Router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './style.css'
 
 import Home from './pages/Home';
@@ -5,13 +7,20 @@ import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
+
       <nav id='header'>
         <a href="/" id='home-link'>
           <h1 id='page-title'>Concert Portfolio</h1>
         </a>
       </nav>
 
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* <Home /> */}
 
       <footer id='footer'>
         <h1>Created by John Climie</h1>
