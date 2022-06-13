224e4456cc74a3f8daa42ff6acd567c4a5306214
John Climie <johnclimie00@gmail.com>
2022-06-13 12:42:53 -0400

    Changed pathnames

--- src/events.js
+++ src/events.js
@@ -11,49 +11,49 @@ const musicEvents = [
         date: 'November 13th, 2018',
         venue: 'State Farm Arena',
         thumbnail: 'http://exclaim.ca/images/travisscott.jpeg',
-        path: 'travis-scott-1',
+        path: 'travisscott1',
     },
     {
         name: 'Travis Scott',
         date: 'March 22nd, 2019',
         venue: 'State Farm Arena',
         thumbnail: 'https://townsquare.media/site/812/files/2018/07/travis-scott-astroworld-cover-art-full.jpg',
-        path: 'travis-scott-2',
+        path: 'travisscott2',
     },
     {
         name: 'Kanye West',
         date: 'September 15th, 2019',
         venue: 'New Birth Missionary Baptist Church',
         thumbnail: 'https://upload.wikimedia.org/wikipedia/en/a/a4/Kanye_West_-_Jesus_Is_King.png',
-        path: 'kanye-west',
+        path: 'kanyewest',
     },
     {
         name: 'James Blake',
         date: 'October 15th, 2021',
         venue: 'The Tabernacle',
         thumbnail: 'https://media.pitchfork.com/photos/60f9880e4a319e50a860a52e/1:1/w_600/James-Blake.jpg',
-        path: 'james-blake'
+        path: 'jamesblake'
     },
     {
         name: 'Tyler, The Creator',
         date: 'March 25th, 2022',
         venue: 'Gas South Arena',
         thumbnail: 'https://resources.tidal.com/images/9db5f3a2/b872/45d6/8dbb/2b7a078be19e/640x640.jpg',
-        path: 'tyler-the-creator',
+        path: 'tylerthecreator',
     },
     {
         name: 'Nine Inch Nails',
         date: 'April 30th, 2022',
         venue: 'Shaky Knees Festival',
         thumbnail: 'http://www.bleaq.com/wp-content/uploads/russel-mills-01.jpg',
-        path: 'nine-inch-nails',
+        path: 'nineinchnails',
     },
     {
         name: 'Phoebe Bridgers',
         date: 'May 27th, 2022',
         venue: 'Cadence Bank Amphitheatre',
         thumbnail: 'https://media.pitchfork.com/photos/5ee923f47bb7acb328d5683d/1:1/w_600/Punisher%20_Phoebe%20Bridgers.jpg',
-        path: 'phoebe-bridgers',
+        path: 'phoebebridgers',
     },
     
 ]
