3489a80c57dd6d63943ceebe5a6079a20540de6b
John Climie <johnclimie00@gmail.com>
2022-08-23 17:32:19 -0400

    Incorporated GIFs and iamges for James Blake

--- src/events.js
+++ src/events.js
@@ -53,7 +53,8 @@ const musicEvents = [
         path: 'jamesblake',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum velit nisi, non dignissim diam rutrum et. Sed vehicula erat a varius aliquet. Ut dignissim purus id posuere euismod. Sed et justo gravida, gravida mauris eget, vestibulum erat. In condimentum ligula vitae magna luctus, vitae porttitor magna consectetur. Praesent dolor quam, mollis eu lacus a, accumsan interdum felis. Fusce a lectus tincidunt, interdum ante et, consequat leo. Curabitur facilisis viverra rhoncus. Donec rutrum ex non nunc fringilla cursus. Morbi sit amet interdum metus, in suscipit enim. Etiam pretium felis nibh, eu placerat odio malesuada nec. Sed nisi est, elementum id justo ac, viverra aliquam libero. Maecenas diam dolor, rhoncus vel tellus eu, euismod lobortis enim. Integer tristique iaculis aliquam. Duis lacinia libero a faucibus aliquam.',
         openers: ['Sabrina Claudio'],
-        guests: ['YFN Lucci']
+        guests: ['YFN Lucci'],
+        images: [require('./images/jamesblake/jamesblake1.jpg'), require('./images/jamesblake/jamesblake2.jpg'), require('./images/jamesblake/jamesblake3.jpg'),  require('./images/jamesblake/jamesblakegif1.gif'), require('./images/jamesblake/jamesblake4.jpg'), require('./images/jamesblake/jamesblake5.jpg'),  require('./images/jamesblake/jamesblakegif2.gif'), require('./images/jamesblake/jamesblake6.jpg'), require('./images/jamesblake/jamesblake7.jpg'), require('./images/jamesblake/jamesblake8.jpg')]
     },
     {
         name: 'Tyler, The Creator',
